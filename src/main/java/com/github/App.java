0ac89df463208a3b0f0b3c026bf4901af748c0ce
indragh <43267246+indragh@users.noreply.github.com>
2018-09-18 17:09:20 +0530

    Update App.java

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args )
     {
     	//Comment
-        System.out.println( "Hello World! --Indranil" );
+        System.out.println( "Hello World--Be Happy" );
     }
 }
 
